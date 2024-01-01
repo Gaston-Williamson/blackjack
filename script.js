c9c0c9c279bc424c93faec0117f3ca5b20215c28
Gaston <91288256+Gaston-Williamson@users.noreply.github.com>
2024-01-01 14:41:09 -0600

    making changes

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ let secondCard = 12
 let hasBlackjack = false
 let isAlive = true
 let message = ""
-
 let sum = firstCard + secondCard
 
-if (sum <= 20){
-    console.log("Do you want to draw a new card?")
-}else if(sum===21){
-    console.log("Whoohoo you win")
-    hasBlackjack = true
-}else {
-    console.log("You lose")
-    isAlive=false
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+function startGame(){
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    }else if(sum===21){
+        message = "Whoohoo you win"
+        hasBlackjack = true
+    }else {
+        message = "You lose"
+        isAlive=false
+    }
+    
+    console.log(message)
 }
 
 
-console.log(hasBlackjack)
-console.log(isAlive)
+
