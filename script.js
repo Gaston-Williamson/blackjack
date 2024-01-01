08c71cdba28a4d5254c74e8e7a7b9c2a245a5446
Gaston <91288256+Gaston-Williamson@users.noreply.github.com>
2024-01-01 11:58:13 -0600

    blackjack

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let firstCard = Math.floor(Math.random() * 10) + 2;
-let secondCard = Math.floor(Math.random() * 10) + 2;
+let firstCard = 10
+let secondCard = 12
+let hasBlackjack = false
+let isAlive = true
+let message = ""
 
 let sum = firstCard + secondCard
 
-comnsole.console.log(sum);
+if (sum <= 20){
+    console.log("Do you want to draw a new card?")
+}else if(sum===21){
+    console.log("Whoohoo you win")
+    hasBlackjack = true
+}else {
+    console.log("You lose")
+    isAlive=false
+}
+
+
+console.log(hasBlackjack)
+console.log(isAlive)
